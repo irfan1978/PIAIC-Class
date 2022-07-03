af258e030d1e5f663709d7a03865aff4d3da00b1
irfan1978 <108614707+irfan1978@users.noreply.github.com>
2022-07-03 16:55:16 +0500

    Add files via upload

---
+++ Assignment_01_even_odd_number.py
@@ -0,0 +1,14 @@
+# *********************************** PYTHON ASSIGNMENT 1 ***********************************
+# Take number as input from the user and tell it is an EVEN or OD number
+# RULE: IF remainder is ZERO then it is an EVEN number, If remainder is not ZERO then it is an ODD number
+
+_input_value = input('Enter any number here, we shall let you know it is an EVEN or ODD number : ')
+result = (int(_input_value) % 2)
+if result == 0:
+    print('The number you enter is an EVEN Number.')
+else:
+    print('The number you enter is an ODD Number.')
+print('***** HAVE A NICE WEEKEND *****')
+
+
+# *********************************** PYTHON ASSIGNMENT 1 ***********************************
